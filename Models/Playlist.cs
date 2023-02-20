1eb363ce15e6720a935176fe2d7b3ec2a14f29a2
Jessica Ejelöv <jessicaejelov@gmail.com>
2023-02-20 20:08:14 +0100

    Model created with class song and genre

---
+++ Models/Playlist.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlaylistAPI.Models
+{
+
+    //class with properties
+    public class Song
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Artist { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        [Required]
+        public int? Length { get; set; }
+
+        //foreign key
+        [Required]
+        [ForeignKey("GenreId")]
+        public int? GenreId { get; set; }
+        public Genre? Genre { get; set; }
+    }
+
+    public class Genre {
+        public int? GenreId { get; set; }
+        [Required]
+        public string? Name { get; set; }
+    }
+}
+
